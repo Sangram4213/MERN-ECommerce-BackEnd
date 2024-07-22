f30760ab2890eda3f93f7c35745f724cd09d6a83
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-07-22 13:09:51 +0530

    connect mongodb to localhost

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import express from "express";
 
-const port = 5000;
+
+
+
+
+//Importing Routes
+import userRoutes from './routes/user.js';
+import { connectDB } from "./utils/features.js";
+
+const port = 4000;
+
+connectDB();
 
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.send("API working with /api/v1");
+})
+
+//Using Routes
+app.use('/api/v1/user',userRoutes);
+
 app.listen(port,()=>{
-    console.log(`Server is workin on http://localhost:${port}`);
+    console.log(`Server is working on http://localhost:${port}`);
 })
