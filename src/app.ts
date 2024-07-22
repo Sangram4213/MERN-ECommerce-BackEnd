f2d48c50c04fe1435f7e0e97ae68fefe5cd8e65f
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-07-22 13:32:21 +0530

    make a user routes, model and controller

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,8 @@ connectDB();
 
 const app = express();
 
+app.use(express.json()); // Automatically parsed JSON
+
 app.get("/",(req,res)=>{
     res.send("API working with /api/v1");
 })
--- src/utils/features.ts
+++ src/utils/features.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   mongoose
     .connect("mongodb://localhost:27017", {
-      dbName: "Ecommerece 24",
+      dbName: "Ecommerece_24",
     })
     .then((c) => console.log(`DB Connected to ${c.connection.host}`))
     .catch((e) => {
