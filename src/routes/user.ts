fca82dfeb7da8f3780ed3a0cdb2ec0f26daabbe1
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-07-27 23:57:16 +0530

    add a authentication changes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
 
 const app = express();
 
@@ -7,10 +8,10 @@ const app = express();
 app.post('/new',newUser);
 
 //route /api/v1/user/all
-app.get('/all',getAllUsers);
+app.get('/all',adminOnly,getAllUsers);
 
 //route /api/v1/user/DynamicID
-app.route('/:id').get(getUser).delete(deleteUser);
+app.route('/:id').get(adminOnly,getUser).delete(adminOnly,deleteUser);
 
 
 export default app;
