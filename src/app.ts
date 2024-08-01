8d70277ad9add3ba52a97bc59f1d202ca8b6c276
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-08-01 17:11:09 +0530

    Make env file for handle confidential info also use morgan for log the info about API

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,45 @@
 import express from "express";
+import morgan from "morgan";
+import logger from "./middlewares/logger.js"
 
 import { connectDB } from "./utils/features.js";
 import { errorMidleware } from "./middlewares/error.js";
 import NodeCache from "node-cache";
+import { config } from "dotenv";
 
 //Importing Routes
 import userRoutes from './routes/user.js';
 import productRoutes from './routes/product.js';
 import orderRoutes from './routes/order.js';
 
+config({
+    path:"./.env"
+});
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-connectDB();
+connectDB(process.env.DB_URI || "");
 
 export const myCache = new NodeCache();
 
 const app = express();
+
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message: string) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 
 app.use(express.json()); // Automatically parsed JSON
 
--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,7 +1,6 @@
 import { TryCatch } from "../middlewares/error.js";
 
 export const newOrder = TryCatch(async(req,res,next)=>{
-   console.log("hello");
    return res.status(200).json({
     success:true,
     message:"Route working proper"
