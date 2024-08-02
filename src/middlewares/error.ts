86eb7d33d3b682dacee8e190633e166812ed5931
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-08-03 01:48:45 +0530

    add CastEoor manually

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -11,6 +11,10 @@ export const errorMidleware = (
   err.message ||= "Internal Server Error";
   err.statusCode ||= 500;
 
+  if(err.name==="CastError"){
+   err.message ="Invalid ID"
+  }
+
   return res.status(err.statusCode).json({
     success: true,
     message: err.message,
