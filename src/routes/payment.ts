55a90b579a483db4f350b04ced56660ef66117dc
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-08-05 20:27:58 +0530

    make a adminRoute in payment routes

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -1,20 +1,21 @@
 import express from 'express';
 import { applyDiscount, deleteCoupon, getAllCoupon, newCoupon } from '../controllers/payment.js';
+import { adminOnly } from '../middlewares/auth.js';
 
 
 const app = express();
 
 //route- /api/v1/payment/coupon/new
-app.post('/coupon/new',newCoupon);
+app.post('/coupon/new',adminOnly,newCoupon);
 
 //route- /api/v1/payment/discount
-app.get('/discount',applyDiscount);
+app.get('/discount',adminOnly,applyDiscount);
 
 //route- /api/v1/payment/coupon/all
-app.get('/coupon/all',getAllCoupon);
+app.get('/coupon/all',adminOnly,getAllCoupon);
 
 //route- /api/v1/payment/coupon/:id
-app.delete('/coupon/:id',deleteCoupon);
+app.delete('/coupon/:id',adminOnly,deleteCoupon);
 
 
 export default app;
